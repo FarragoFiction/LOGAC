3a1efd079b17c00211cacee8083d2fe29b2db2e1
jadedResearcher <gtg287y@gmail.com>
2020-01-20 01:09:01 -0500

    updated tests

--- web/main.dart
+++ web/main.dart
@@ -12,57 +12,16 @@ void main() async {
   await Item.slurpItems();
   //Item.debugAllInDom(querySelector("#output"));
   ItemBuilder.go(querySelector("#output"));
-  //test();
+  test();
 }
 
 void test() {
   DivElement output = querySelector("#output");
-  List<Rule> ruleSet1 = new List<Rule>();
-  ruleSet1.add(new PermissiveRule("you can jump"));
-  ruleSet1.add(new PermissiveRule("you can throw the object"));
-  ruleSet1.add(new PermissiveRule("you can bounce the object"));
-  ruleSet1.add(new PermissiveRule("you can take the object from others"));
-  ruleSet1.add(new RestrictiveRule("you can't move while you have the object"));
-  ruleSet1.add(new RestrictiveRule("you can't fight"));
-  ruleSet1.add(new RestrictiveRule("you can't throw the object at your own side "));
-  ruleSet1.add(new RestrictiveRule("you can't leave the field"));
 
-
-  List<Rule> ruleSet2 = new List<Rule>();
-  ruleSet2.add(new PermissiveRule("you can eat the object"));
-  ruleSet2.add(new PermissiveRule("you can throw the object"));
-  ruleSet2.add(new PermissiveRule("you can cut the object"));
-  ruleSet2.add(new PermissiveRule("you can light fires on the object"));
-  ruleSet2.add(new RestrictiveRule("you can't steal the object"));
-  ruleSet2.add(new RestrictiveRule("you cannot have 40 of the object"));
-  ruleSet2.add(new RestrictiveRule("you cannot bounce the object"));
-  ruleSet2.add(new RestrictiveRule("you cannot wash the object"));
-
-  List<Rule> ruleSet3 = new List<Rule>();
-  ruleSet3.add(new PermissiveRule("you can draw up to 7 of the object"));
-  ruleSet3.add(new PermissiveRule("you win when your opponent's object is dead"));
-  ruleSet3.add(new PermissiveRule("you can have up to three objects in play"));
-  ruleSet3.add(new PermissiveRule("you must tap the object to play it"));
-  ruleSet3.add(new RestrictiveRule("you can't interact with your opponents object directly"));
-  ruleSet3.add(new RestrictiveRule("you can't break an egg before its ready"));
-  ruleSet3.add(new RestrictiveRule("you lose points if you don't care for your object"));
-  ruleSet3.add(new RestrictiveRule("you cannot ignore the object"));
-
-  List<Rule> ruleSet4 = new List<Rule>();
-  ruleSet4.add(new PermissiveRule("you can water the object three times a week"));
-  ruleSet4.add(new PermissiveRule("you can harvest the object at the end of the season"));
-  ruleSet4.add(new PermissiveRule("you can eat the object"));
-  ruleSet4.add(new PermissiveRule("you can cut the object"));
-  ruleSet4.add(new RestrictiveRule("you can't steal the object"));
-  ruleSet4.add(new RestrictiveRule("you cannot ignore the object"));
-  ruleSet4.add(new RestrictiveRule("you cannot bounce the object"));
-  ruleSet4.add(new RestrictiveRule("you cannot burn the object"));
-
-
-  Item first = new Item("Basketball", new Set.from(ruleSet1));
-  Item second = new Item("Cake", new Set.from(ruleSet2));
-  Item third = new Item("Fiduspawn Cards", new Set.from(ruleSet3));
-  Item fourth = new Item("Carrot", new Set.from(ruleSet4));
+  Item first = Item.items[0];
+  Item second = Item.items[1];
+  Item third = Item.items[2];
+  Item fourth = Item.items[3];
   first.debugInDOM(output);
   second.debugInDOM(output);
   AlchemyResult resultOR = new AlchemyResultAND(<Item>[first,second])..result;
