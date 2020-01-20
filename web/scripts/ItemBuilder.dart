e21caf8eb663976232051f6ad61de7a20566d3ed
jadedResearcher <gtg287y@gmail.com>
2020-01-20 00:37:14 -0500

    basic builder not wired up

--- web/scripts/ItemBuilder.dart
+++ web/scripts/ItemBuilder.dart
@@ -25,16 +25,32 @@ abstract class ItemBuilder {
 
     static void makeBuilder(Item item, Element element) {
         DivElement div = new DivElement()
-            ..text = item.baseName
             ..style.border="1px solid black";
+
+        TextInputElement input = new TextInputElement()..value = item.baseName;
+        div.append(input);
 
         int i = 0;
         for(Rule rule in item.rules) {
             i++;
-            div.append(new DivElement()..text = "$i: ${rule.applyObjectToPhraseDebug(item.baseName)}");
+            DivElement ruleDiv = new DivElement()..text = "Rule $i:";
+            div.append(ruleDiv);
+            SelectElement select = addRuleDropDown(item,Rule.rules.indexOf(rule));
+            div.append(select);
+            //TODO do onchange
         }
 
         element.append(div);
+    }
+
+    static SelectElement addRuleDropDown(Item item, int selectedIndex) {
+        SelectElement ret = new SelectElement();
+        for(int i = 0; i<Rule.rules.length; i++) {
+            Rule rule = Rule.rules[i];
+            OptionElement option = new OptionElement(value: i.toString(), selected: selectedIndex==i)..text=rule.toString();
+            ret.append(option);
+        }
+        return ret;
     }
 
     static void debugItemInDOM(Item item, Element element) {
